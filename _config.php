6cb8b90e283cd73fe3b2ff1633dded6d2f81a11b
Lucas Pélos <lucas.pelos@gmail.com>
2015-01-26 23:10:10 -0200

    assets/ and assets/images to default image_paths

--- _config.php
+++ _config.php
@@ -1,5 +1,10 @@
 <?php
 
+SSImageHelpersExtension::add_image_path(array(
+  'assets/',
+  'assets/images/',
+));
+
 if ($current_theme = SSViewer::current_theme()) {
   SSImageHelpersExtension::add_image_path('themes/' . $current_theme . '/images/');
 }
